a850acc11bafe6b3d21dcb9a72dd22ff61de175f
SixGod <sixgod@SixGoddeMacBook-Air.local>
2023-03-27 02:05:36 +0900

    cow

---
+++ kernel/kalloc.c
@@ -0,0 +1,114 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+int ref_count[PHYSTOP/PGSIZE];
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    ref_count[(uint64)p/PGSIZE]=1;
+    kfree(p);
+  }
+}
+
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  acquire(&kmem.lock);
+  int idx = (uint64)pa / PGSIZE;
+  ref_count[idx]--;
+  int ref = ref_count[idx];
+  release(&kmem.lock);
+
+  if(ref < 0)
+    panic("kfree error!\n");
+
+  if(ref > 0)
+    return;
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, PGSIZE);
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r){
+    int idx = (uint64) r / PGSIZE;
+    if(ref_count[idx] != 0)
+      panic("invalid ref count!\n");
+    ref_count[idx] = 1;
+    kmem.freelist = r->next;
+  }
+  release(&kmem.lock);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
+}
+
+void incref(uint64 pa)
+{
+  int idx = pa/PGSIZE;
+  acquire(&kmem.lock);
+  if(pa > PHYSTOP || ref_count[idx] < 1)
+    panic("incref");
+  
+  ref_count[idx]+=1;
+  release(&kmem.lock);
+}
